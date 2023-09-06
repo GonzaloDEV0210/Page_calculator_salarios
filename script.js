ab4171bd145e5bed644f5f7b6c618addb2e6d43f
Gonzalop0210 <gonzalopalmadev0210@gmail.com>
2023-09-05 20:13:58 -0500

    obtener el data-value del html

---
+++ script.js
@@ -0,0 +1,30 @@
+const input_operacion = document.querySelector("#operacion");
+const input_resultado = document.querySelector("#resultado");
+
+const e = document.querySelector(".signo_e");
+const porcentaje = document.querySelector(".signo_porcentaje");
+const sin = document.querySelector(".signo_sin");
+const deg = document.querySelector(".signo_deg");
+const c = document.querySelector(".operator_clear");
+const parentesis1 = document.querySelector(".parentesis1");
+const parentesis2 = document.querySelector(".parentesis2");
+const division = document.querySelector(".signo_division");
+const number_7 = document.querySelector(".number_7");
+const number_8 = document.querySelector(".number_8");
+const number_9 = document.querySelector(".number_9");
+const signo_x = document.querySelector(".signo_x");
+const number_4 = document.querySelector(".number_4");
+const number_5 = document.querySelector(".number_5");
+const number_6 = document.querySelector(".number_6");
+const signo_menos = document.querySelector(".signo_menos");
+const number_1 = document.querySelector(".number_1");
+const number_2 = document.querySelector(".number_2");
+const number_3 = document.querySelector(".number_3");
+const signo_mas = document.querySelector(".signo_mas");
+const number_cero = document.querySelector(".number_cero");
+const number_doble_cero = document.querySelector(".number_doble_cero");
+const signo_punto = document.querySelector(".signo_punto");
+const signo_equals = document.querySelector(".signo_equals");
+
+console.log(e.getAttribute("data-value"));
+console.log(porcentaje.getAttribute("data-value"))
