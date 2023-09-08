0712c66aba6bfdc75f93b46399eac1ce234b716c
Gonzalop0210 <gonzalopalmadev0210@gmail.com>
2023-09-07 21:47:22 -0500

    Estilizacion del apartado moda casi terminado

--- js/theme.js
+++ js/theme.js
@@ -5,6 +5,7 @@ const link_item_nav_cal = document.querySelector(".link_item_nav_cal");
 const link_item_nav_mod = document.querySelector(".link_item_nav_mod");
 const link_item_nav_med = document.querySelector(".link_item_nav_med");
 const link_item_nav_pro = document.querySelector(".link_item_nav_pro");
+const container_results = document.querySelector(".container_results");
 
 icon_theme.addEventListener("click", function(){
     body.classList.toggle("body_dark");
@@ -13,4 +14,5 @@ icon_theme.addEventListener("click", function(){
     link_item_nav_mod.classList.toggle("link_item_nav_mod_dark");
     link_item_nav_med.classList.toggle("link_item_nav_med_dark");
     link_item_nav_pro.classList.toggle("link_item_nav_pro_dark");
+    container_results.classList.toggle("container_results_dark");
 });
