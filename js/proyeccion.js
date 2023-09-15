6edc9ebd90f85274e73c9e184f99466f877e4ee6
Gonzalo Palma <gonzalopalmadev0210@gmail.com>
2023-09-15 17:01:29 -0500

    Salarios agregados en un array, falta hacer la operación

--- js/proyeccion.js
+++ js/proyeccion.js
@@ -1,45 +1,53 @@
-const iconAdd = document.querySelector(".icon_add");
-const form = document.querySelector(".form");
-const containerIconAdd = document.querySelector(".container_icon_add");
-const btnLimpiar = document.querySelector(".btnLimpiar");
-const btnResolver = document.querySelector(".btnResolver");
-const input = document.querySelectorAll("input"); /* el input recibe una lista con todos los elementos que tienen la misma clase  */
-const txtRespuesta = document.querySelector(".txtRespuesta_dark");
+document.addEventListener('DOMContentLoaded', function () {
+    const iconAdd = document.querySelector(".icon_add");
+    const form = document.querySelector(".form");
+    const containerIconAdd = document.querySelector(".container_icon_add");
+    const btnLimpiar = document.querySelector(".btnLimpiar");
+    const btnResolver = document.querySelector(".btnResolver");
+    const txtRespuesta = document.querySelector(".txtRespuesta_dark");
 
-let click = 0;
-iconAdd.addEventListener("click", function(event){
-    if (click < 3) {
-        let newDivAdd = document.createElement("div");
-        newDivAdd.innerHTML = "<label class='text_input'>Ingresa salario:</label><input class='input' type='text'>";
-        newDivAdd.classList.add('container_label_input');
-        form.appendChild(newDivAdd);
-        click++;
-        if (click == 3) {
-            containerIconAdd.classList.add("icon_add_none");
-        };
-    };
-});
-
-btnLimpiar.addEventListener("click", function(){
-    txtRespuesta.textContent = "";
-    input.forEach(function(elemnto){ /* recorremos la lista input (funciona como un arreglo pero no es un arreglo) */
-        elemnto.value = "";
-    });
-});
-
-
-
-btnResolver.addEventListener("click", function(){
-    let alertMostrada = false;
-    input.forEach(function(){
-        if(input[0].value.length > 0) {
-            txtRespuesta.value = input[0].value;
-        } else {
-            if (!alertMostrada) {
-                alert("Escriba su nombre o el de su empresa");
-                alertMostrada = true;
+    let click = 0;
+    iconAdd.addEventListener("click", function (event) {
+        if (click < 3) {
+            let newDivAdd = document.createElement("div");
+            newDivAdd.innerHTML = "<label class='text_input'>Ingresa salario:</label><input class='input' type='text'>";
+            newDivAdd.classList.add('container_label_input');
+            form.appendChild(newDivAdd);
+            click++;
+            if (click == 3) {
+                containerIconAdd.classList.add("icon_add_none");
             };
         };
-        let index = 1; /* Decirle que a partir del index uno me guarde los salarios en un array */
+    });
+
+    btnLimpiar.addEventListener("click", function () {
+        const input = document.querySelectorAll(".input"); /* el input recibe una lista con todos los elementos que tienen la misma clase  */
+        txtRespuesta.value = "";
+        input.forEach(function (elemnto) { /* recorremos la lista input (funciona como un arreglo pero no es un arreglo) */
+            elemnto.value = "";
+        });
+    });
+
+    btnResolver.addEventListener("click", function () {
+        const input = document.querySelectorAll(".input"); /* el input recibe una lista con todos los elementos que tienen la misma clase  */
+        let alertMostrada = false;
+        input.forEach(function () {
+            if (input[0].value.length > 0) {
+                txtRespuesta.value = input[0].value;
+            } else {
+                if (!alertMostrada) {
+                    alert("Escriba su nombre o el de su empresa");
+                    alertMostrada = true;
+                };
+            };
+        });
+
+        let arraySalarios = [];
+        for (let i = 1; i < input.length; i++) {
+            if (input[i].value != 0) {
+                arraySalarios.push(Number(input[i].value));
+            };
+        };
+        console.log(arraySalarios);
     });
 });
